631d72ffa02bd73de26ca582ff97ecb915a66bf9
Gudkova Lena <gudkova_elena1992@mail.ru>
2021-06-06 07:30:35 +0300

    Lesson2 and HomeWork2 Примитивные типы данных. Понятие "переменная". Системы счисления

---
+++ src/HomeWork2/App.java
@@ -0,0 +1,49 @@
+package HomeWork2;
+
+public class App {
+    public static void main(String[] args) {
+    //десятиричная
+    byte b1 = 12;
+    short s1 = 1300;
+    int i = 0;
+    long l = 123456789L;
+    float f = 12.5f;
+    double d = 12.4;
+    float f2 = 12;
+    double d2= 12.4d;
+
+    char c = '1';
+    char c1 = 500;
+    char c2 = '\u05AB';
+
+    boolean bol = true;
+
+    //восьмиричная
+        byte b2 = 012;
+        short s2 = 4570;
+        int i2 = 0;
+        long l2 = 726_746_425;
+
+    // двоичная
+        byte b3 = 0b1100;
+        short s3 = 0b0101_0001_0100;
+        int i3 = 0;
+        long l3 = 0b0111_0101_1011_1100_1101_0001_0101;
+
+    // шестнадцатиричная
+        byte b4 = 0xc;
+        short s4 = 0x978;
+        int i4 = 0;
+        long l4 = 0x75BCD15;
+
+
+
+    int a1 = 200000000;
+    int a2 = 200000000;
+    int result = a1 + a2;
+    int result2 = 200000000 + 200000000;
+
+
+        System.out.println(result2);
+    }
+}
